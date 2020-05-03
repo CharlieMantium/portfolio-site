03876d18fc8698183311f58e09599797e7570809
CharlieMantium <karol.zieniewicz@gmail.com>
2020-05-03 21:38:16 +0200

    tried another config 2

--- src/components/Section/SectionWrapper.js
+++ src/components/Section/SectionWrapper.js
@@ -8,7 +8,7 @@ import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
 
 import { breakPoints } from 'styles/base';
 
-ScrollMagicPluginGsap(ScrollMagic, gsap);
+if (ScrollMagicPluginGsap) ScrollMagicPluginGsap(ScrollMagic, gsap);
 
 const SectionWrapper = styled.div`
   min-height: 100vh;
